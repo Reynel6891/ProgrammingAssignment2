6325372c0947620e53c3a980efba5bffd69c0a7f
Reynel Urrea Castellanos <rey.urrea@gmail.com>
2016-10-03 00:07:43 +0200

    assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I will create a series of functions that allow to retrieve the inverse of a user-defined 
+## matrix if previously calculated in the global environment.  
 
-## Write a short comment describing this function
+
+## first, i create a function that calculates and stores in a vector the inverse of any matrix  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_1 <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse_1 <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) inverse_1 <<- inverse
+  getInverse <- function() inverse_1
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Second, i create a function that search if the inverse of the user-defined matrix
+## has been already caculated inside the environment.  
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inverse_1 <- x$getInverse()
+  if (!is.null(inverse_1)) {
+    message("cached data")
+    return(inverse_1)
+  }
+  mat <- x$get()
+  inverse_1 <- solve(mat, ...)
+  x$setInverse(inverse_1)
+  inverse_1
 }
